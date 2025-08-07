da58234d66a314e0887f2c3412ab19bbab90e6df
thanhhoan99 <thanhhoan7878647@gmail.com>
2025-08-07 09:45:37 +0700

    add

--- movie/src/docker/init-sql/data.sql
+++ movie/src/docker/init-sql/data.sql
@@ -1,3 +1,59 @@
+DROP DATABASE IF EXISTS movies;
+CREATE DATABASE movies CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE movies;
+
+
+
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+REPLACE  IGNORE INTO `roles` (`id`, `name`) VALUES (1,'Administrators'),(2,'Managers'),(3,'Users');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-08-07  9:30:32
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: movies
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users_roles`
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: movies
@@ -37,7 +93,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'$2a$10$X0cr/v.Ck7yuJvtKpdKXV.afq1JACXESEYnWoVt2ZqJxkee.vMbXa','newmanager',NULL),(2,'$2a$10$WAX5kbzZQIbonWuj8Sc5HOCxkwf/yuQ0ENpd896QNBNQkTvGlPXwS','Admin',NULL),(3,'$2a$10$v1xy7u/DRiM8vTS6jtxo7eji2/RUIX/fucSdQKhErvQpU.6SzCvhu','newuser',NULL),(4,'$2a$10$eev9Uj6lp.Jm/t23v5y0E.XpBSGUbNpdzPRnG6/xchUkeC1Gxsbfm','newuser1',NULL),(5,'$2a$10$tkZmdJtOuso9LD5o6GgHMulr0yHl4Ui41vF3O9aOaUwMmZIuM9QwW','thanhhoan',NULL),(6,'$2a$10$ciHw3u77pCV9ZeO8j7U3ze1wFKE4noFPHT04j0MgdsVtdQYDZ9p.K','thanhhoan99',NULL),(7,'$2a$10$m/24PCoKMzdhGgm3Q..gee1WC7lDGaeMxezEIyd2cTRrZP5ikHgCG','Administrators',NULL),(8,'$2a$10$Ie2JTcXOHTdTPkMIJSt/RuvmO1fFAqzlHm4g6CzIpCdEAdq0.A43G','2Role',NULL);
+REPLACE  IGNORE INTO `users` (`id`, `password`, `username`, `is_active`) VALUES (1,'$2a$10$X0cr/v.Ck7yuJvtKpdKXV.afq1JACXESEYnWoVt2ZqJxkee.vMbXa','newmanager',NULL),(2,'$2a$10$WAX5kbzZQIbonWuj8Sc5HOCxkwf/yuQ0ENpd896QNBNQkTvGlPXwS','Admin',NULL),(3,'$2a$10$v1xy7u/DRiM8vTS6jtxo7eji2/RUIX/fucSdQKhErvQpU.6SzCvhu','newuser',NULL),(4,'$2a$10$eev9Uj6lp.Jm/t23v5y0E.XpBSGUbNpdzPRnG6/xchUkeC1Gxsbfm','newuser1',NULL),(5,'$2a$10$tkZmdJtOuso9LD5o6GgHMulr0yHl4Ui41vF3O9aOaUwMmZIuM9QwW','thanhhoan',NULL),(6,'$2a$10$ciHw3u77pCV9ZeO8j7U3ze1wFKE4noFPHT04j0MgdsVtdQYDZ9p.K','thanhhoan99',NULL),(7,'$2a$10$m/24PCoKMzdhGgm3Q..gee1WC7lDGaeMxezEIyd2cTRrZP5ikHgCG','Administrators',NULL),(8,'$2a$10$Ie2JTcXOHTdTPkMIJSt/RuvmO1fFAqzlHm4g6CzIpCdEAdq0.A43G','2Role',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,8 +106,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-06 14:53:54
-
+-- Dump completed on 2025-08-07  9:30:32
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: movies
@@ -70,7 +125,9 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `users_roles`
+-- Table structure for table `roles`
+--
+
 --
 
 DROP TABLE IF EXISTS `users_roles`;
@@ -92,7 +149,7 @@ CREATE TABLE `users_roles` (
 
 LOCK TABLES `users_roles` WRITE;
 /*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
-INSERT INTO `users_roles` VALUES (7,1),(8,3),(1,2),(3,2),(3,3),(4,2),(2,3),(5,3),(6,2);
+REPLACE  IGNORE INTO `users_roles` (`user_id`, `role_id`) VALUES (7,1),(8,3),(1,2),(3,2),(3,3),(4,2),(2,3),(5,3),(6,2);
 /*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,5 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-06 14:53:54
-
+-- Dump completed on 2025-08-07  9:30:32
